fedba29f9b2b64f14cf1efe70ed15b5bf7fc3855
111kalyani <89124574+111kalyani@users.noreply.github.com>
2021-08-29 18:17:16 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,53 @@
+
+var resume ={
+    "basics": {
+      "name": "kalyani pawar;",
+      "label": "full stack developer",
+      "email": "pawarkalyani111@gmail.com",
+      "phone": "123456789",
+      "location": {
+        "address": "27 camp house",
+        "postalCode": "111222",
+        "city": "amravati",
+        "countryCode": "india",
+        "region": "maharashtra"
+      },
+      
+    },
+    
+    "education": [{
+      "institution": "University",
+      "area": "computer science and engineering",
+      "studyType": "Bachelor of engineering",
+      "startDate": "2016-01-01",
+      "endDate": "2020-01-01",
+      "score": "9.18",
+    }],
+    
+    "skills": [{
+      "name": "Web Development",
+      "level": "intermediate",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages": [{
+      "language": "English",
+      "fluency": "advance",
+      "language": "marathi",
+      "fluency": "native speaker"
+    }],
+    
+    "projects": [{
+      "name": "Project",
+      "description": "Description…",
+      "keywords": [
+        "HTML"
+      ],
+      
+    }]
+  }
+
+  console.log(resume);
